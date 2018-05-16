8119ae877a8b0c644605d49b093251c651baf0ac
unl0v3d <36156250+unl0v3d@users.noreply.github.com>
2018-05-16 09:41:33 +0200

    ootify backend

--- imports/api/marketmaker/update.js
+++ imports/api/marketmaker/update.js
@@ -5,14 +5,15 @@ import { Tradedata } from '../../api/tradedata/tradedata.js';
 Meteor.setInterval(function() {
     if(Userdata.find().count() > 4) {
         Meteor.call('getbalance', 'KMD');
-        Meteor.call('getbalance', 'BNTN');
+        Meteor.call('getbalance', 'OOT');
         Meteor.call('getbalance', 'BTC');
-        Meteor.call('getbalance', 'QTUM');
+        Meteor.call('getbalance', 'LTC');
+        Meteor.call('getbalance', 'ZEC');
     }
     if(Userdata.find().count() > 4 && Tradedata.find().count() > 0) {
     Meteor.call('getprice', 'KMD');
     Meteor.call('getprice', 'BTC');
-    Meteor.call('getprice', 'QTUM');
+    Meteor.call('getprice', 'LTC');
     Meteor.call("checkswapstatus");
     }
 }, 60000);
@@ -20,8 +21,9 @@ Meteor.setInterval(function() {
 Meteor.setInterval(function() {
     if(Userdata.find().count() > 6) {
         Meteor.call('listtransactions', "KMD");
-        Meteor.call('listtransactions', "QTUM");
-        Meteor.call('listtransactions', "BNTN");
+        Meteor.call('listtransactions', "LTC");
+        Meteor.call('listtransactions', "OOT");
+        Meteor.call('listtransactions', "ZEC");
         Meteor.call('listtransactions', "BTC");
     }
 
